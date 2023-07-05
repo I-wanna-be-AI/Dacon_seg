afa349a5bf748ca0a92410f6cfc167c392fb56bc
Donghyun Kim <dk58319@ajou.ac.kr>
2023-07-05 20:34:25 +0900

    ADD: new model, resnext

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Train
-torchrun --nproc_per_node=2 main.py --gpu_ids 8,9 --train 1 --img_size 224 --model unet_base --batchsize 1024 --save_name unet_base
+torchrun --nproc_per_node=2 main.py --gpu_ids 8,9 --train 1 --img_size 224 --model unet_base --batchsize 1024 --save_name unet_base --one_cycle_max_lr 1e-4 --epochs 30
 
 
 # Inference
